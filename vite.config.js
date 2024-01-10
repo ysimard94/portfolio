3883fef6f38fbc7527d003bb22371167d5ed80d1
ysimard <yannick.simard94@gmail.com>
2024-01-10 12:13:35 -0500

    Fixed missing import

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+
+// Helper function to get dirname
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+console.log('__dirname:', __dirname + 'src/locale/'); 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueI18nPlugin({
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locale/**'),
+      include: path.resolve(__dirname, 'src/locale/**'), // Use path.resolve here
     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': path.resolve(__dirname, 'src'), // Use path.resolve here
+    },
+  },
+});
