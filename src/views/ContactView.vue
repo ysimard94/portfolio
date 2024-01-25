6f35dfb372f7103725b57fcf9ca7d09c3e674a47
ysimard <yannick.simard94@gmail.com>
2024-01-24 22:23:52 -0500

    netlify test

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,40 @@
+<script>
+export default {
+    name: "ContactView",
+    methods: {
+        encode(){
+
+        },
+        handleSubmit() {
+            fetch("/", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams(new FormData(document.forms.contact)),
+            })
+                .then(() => {
+                    alert("Success!");
+                })
+                .catch((error) => alert(error)
+        },
+    },
+};
+</script>
 <template>
     <section>
         <form
             name="contact"
             method="POST"
+            action="/about"
+            netlify-honeypot="bot-field"
             data-netlify="true"
             onsubmit="submit"
         >
+            <p class="hidden">
+                <label>
+                    Don't fill this out if you're human:
+                    <input name="bot-field" />
+                </label>
+            </p>
             <p>
                 <label>Name <input type="text" name="name" /></label>
             </p>
@@ -18,3 +47,8 @@
         </form>
     </section>
 </template>
+<style>
+.hidden {
+    display: none;
+}
+</style>
