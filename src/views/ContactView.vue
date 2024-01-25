d3657126a1175cd54043392dbbd912e7c68df0b2
ysimard <yannick.simard94@gmail.com>
2024-01-24 22:49:47 -0500

    netlify form test

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,48 @@
+<script>
+export default {
+    name: "ContactView",
+    data: () => ({
+        form: {
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+        },
+    }),
+    methods: {
+        encode(data) {
+            return Object.keys(data)
+                .map(
+                    (key) =>
+                        `${encodeURIComponent(key)}=${encodeURIComponent(
+                            data[key],
+                        )}`,
+                )
+                .join("&");
+        },
+        handleSubmit() {
+            fetch("/", {
+                method: "post",
+                headers: { "Content-Type": "application/x-www-urlencoded" },
+                body: this.encode({
+                    "form-name": "contact",
+                    ...this.form,
+                }),
+            })
+                .then(() => console.log("successfully sent"))
+                .catch((e) => console.error(e));
+        },
+    },
+};
+</script>
 <template>
     <section>
         <form
+            @submit.prevent="handleSubmit"
             name="contact"
             method="POST"
-            action="/about"
             netlify-honeypot="bot-field"
             data-netlify="true"
-            onsubmit="submit"
         >
             <p class="hidden">
                 <label>
@@ -15,10 +51,32 @@
                 </label>
             </p>
             <p>
-                <label>Name <input type="text" name="name" /></label>
+                <label
+                    >Name <input v-model="form.name" type="text" name="name"
+                /></label>
             </p>
             <p>
-                <label>Email <input type="email" name="email" /></label>
+                <label
+                    >Email
+                    <input v-model="form.email" type="email" name="email"
+                /></label>
+            </p>
+            <p>
+                <label
+                    >Subject
+                    <input v-model="form.subject" type="text" name="subject"
+                /></label>
+            </p>
+            <p>
+                <label
+                    >Message
+                    <textarea
+                        v-model="form.message"
+                        type="text"
+                        name="message"
+                        rows="2"
+                    />
+                </label>
             </p>
             <p>
                 <button type="submit">Send</button>
